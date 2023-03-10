079e25a34982ebf7787ea0e84b48f50bbe1c558d
LDS <108324169+luanddtdev@users.noreply.github.com>
2023-03-10 16:10:48 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { styles } from '../sass'
+import { FiSearch } from 'react-icons/fi'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={`${styles.paddingX} py-6 shadow-navbar`}>
+            <div className={`${styles.innerWidth} mx-auto flex justify-between items-center`}>
+                <h2 className='font-extrabold text-[24px] text-secondary uppercase'>
+                    FPT BOOK
+                </h2>
+
+                <div className='flex bg-white p-2 rounded-[10px]'>
+                    <input 
+                        type='text'
+                        placeholder='Search...'
+                        className='sm:w-[300px] w-[150px] text-secondary placeholder:text-secondary border-none outline-none'
+                    />
+
+                    <button
+                        type='button'
+                        className='text-[24px] text-secondary cursor-pointer'
+                    >
+                        <FiSearch />
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#755139FF'
+        'primary': '#755139FF',
+        'secondary': '#D4B996FF'
+      },
+      boxShadow: {
+        'navbar': '0 5px 10px #D4B996FF'
       }
     },
   },
